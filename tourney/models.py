7536e3fd46bff258d2d995178a60bd7eb5392177
gireeshvm <marathigireesh@gmail.com>
2019-07-09 07:32:33 +0530

    Added More detail page to tourney detail page

--- tourney/models.py
+++ tourney/models.py
@@ -10,7 +10,8 @@ class Tournament(models.Model):
     tourney_type = models.CharField(max_length=20)
     price = models.IntegerField()
     time = models.DateTimeField()
-
+    tourney_id = models.CharField(max_length=50)
+    tourney_pass = models.CharField(max_length=15)
 
     def __str__(self):
         return self.game.name
